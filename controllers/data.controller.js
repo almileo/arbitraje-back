9146a68dd2f21c874540d4900161638ea320298a
Mauro Gaston Perez Gianolini <pmora92@hotmail.com>
2023-09-28 10:38:46 -0300

    Realizacion peticion Huobi, CryptoDotCom y GateIo

--- controllers/data.controller.js
+++ controllers/data.controller.js
@@ -72,7 +72,7 @@ const getDataBybit = async (req, res, next) => {
   const dataBybit = await axios.get(ConstantURL.bybit.url)
 
   let bybitDataArr = dataBybit.data.result;
-  console.log('bybitdataArr: ', dataBybit.data.result);
+  let status = parseInt(dataBybit.data.ret_code);
   bybitDataArr.forEach(e => {
     let url = '';
     let base = e.symbol.slice(-4);
@@ -90,10 +90,86 @@ const getDataBybit = async (req, res, next) => {
     e.ask = parseFloat(e.ask_price);
   })
   const filteredData = hasFailedSymbols(comprobatedSymbols, bybitDataArr)
-  console.log('FilteredData: ', filteredData);
-  
+  if (status === 0) {
+    return res.json(filteredData).status(200);
+  } else {
+    return res.json(statusText).status(status);
+  }
+
 }
 
 
+const getDataHuobi = async (req, res, next) => {
+  const dataHuobi = await axios.get(ConstantURL.houbi.url);
+  const huobiDataArr = dataHuobi.data.data;
+  const status = dataHuobi.data.status;
+  console.log('status: ', status);
+  huobiDataArr.forEach(e => {
+    let base = e.symbol.slice(-4);
+    let url = '';
+    if (base === 'usdt' || base === 'usdc' || base === 'usdd' || base === 'husd') {
+      url = `${e.symbol.slice(0, -4)}${'_'}${base}`;
+    } else {
+      url = `${e.symbol.slice(0, -3)}${'_'}${e.symbol.slice(-3)}`;
+    }
+    e.url = `https://www.huobi.com/es-es/exchange/${url}`;
+    e.symbol = e.symbol.toUpperCase();
+    e.price = e.close;
+    let isComprobated = comprobatedSymbols.includes(e.symbol);
+    e.isComprobated = isComprobated;
+    e.volume = e.vol;
+    e.bid = parseFloat(e.bid);
+    e.ask = parseFloat(e.ask);
+  })
+  const filteredData = hasFailedSymbols(failHuobi, huobiDataArr)
+  if (status === 'ok') {
+    return res.json(filteredData).status(200);
+  } else {
+    return res.json(statusText).status(status);
+  }
+}
 
-module.exports = { getDataBinance, getDataKucoin, getDataBybit }
+const getDataCryptoDotCom = async (req, res, next) => {
+  const dataCryptoDotCom = await axios.get(ConstantURL.cryptoDotCom.url);
+  const cryptoDotComDataArr = dataCryptoDotCom.data.result.data;
+  const status = parseInt(dataCryptoDotCom.data.code);
+  cryptoDotComDataArr.forEach(e => {
+    e.url = `https://crypto.com/exchange/trade/spot/${e.i}`;
+    e.symbol = e.i.replace(/_/, '');
+    e.price = e.a;
+    let isComprobated = comprobatedSymbols.includes(e.symbol);
+    e.isComprobated = isComprobated;
+    e.volume = e.vv;
+    e.bid = parseFloat(e.b);
+    e.ask = parseFloat(e.k);
+  })
+  const filteredData = hasFailedSymbols(comprobatedSymbols, cryptoDotComDataArr)
+  if (status === 0) {
+    return res.json(filteredData).status(200);
+  } else {
+    return res.json(statusText).status(status);
+  }
+}
+
+const getDataGateIo = async (req, res, next) => {
+  const dataGateIo = await axios.get(ConstantURL.gate_io.url)
+  const gateIoDataArr = dataGateIo.data
+  gateIoDataArr.forEach(e =>{
+    e.url = `https://www.gate.io/es/trade/${e.currency_pair}`;
+    e.symbol = e.currency_pair.replace(/_/, '');
+    e.price = e.last;
+    let isComprobated = comprobatedSymbols.includes(e.symbol);
+    e.isComprobated = isComprobated;
+    e.volume = e.base_volume;
+    e.bid = parseFloat(e.highest_bid);
+    e.ask = parseFloat(e.lowest_ask);
+  })
+  const filteredData = hasFailedSymbols(failGateio, gateIoDataArr)
+  return res.json(filteredData).status(200);
+    
+    
+
+}
+
+
+module.exports = { getDataBinance, getDataKucoin, getDataBybit, getDataHuobi, getDataCryptoDotCom, getDataGateIo }
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const { getDataBinance, getDataKucoin, getDataBybit } = require('../controllers/data.controller');
+const { getDataBinance, getDataKucoin, getDataBybit, getDataHuobi, getDataCryptoDotCom, getDataGateIo } = require('../controllers/data.controller');
 const cors = require('cors');
 
 
-router.get('/',cors(), getDataBybit);
+router.get('/',cors(), getDataGateIo);
 
 
 module.exports = router;
